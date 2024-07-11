46f87a5d6a14cc4129b1eb7926e6f6e370aa414a
CyanEbi <cyanebi@proton.me>
2024-07-11 23:18:06 +0200

    Basic randomizer is working

--- drawaboxwarmup.js
+++ drawaboxwarmup.js
@@ -1,11 +1,15 @@
 const select = document.getElementById("dropdown");
+const result = document.getElementById("result");
+let lessons;
+let nlessons;
+let exercisePools;
 
 fetchExercises();
 
 async function fetchExercises() {
     const response = await fetch("exercises.json");
-    const lessons = await response.json();
-    const nlessons = lessons.length;
+    lessons = await response.json();
+    nlessons = lessons.length;
 
     for (let i = 0; i < nlessons; i++) {
         const option = document.createElement("option");
@@ -13,4 +17,20 @@ async function fetchExercises() {
         select.add(option);
     }
     
+    generateExercisePools();
+}
+
+function generateExercisePools() {
+    exercisePools = Array(nlessons);
+    exercisePools[0] = lessons[0].exercises;
+
+    for (let i = 1; i < nlessons; i++) {
+        exercisePools[i] = exercisePools[i-1].concat(lessons[i].exercises);
+    }
+}
+
+function pickWarmup() {
+    const idx = select.selectedIndex;
+    const len = exercisePools[idx].length;
+    result.innerText = exercisePools[idx][Math.floor(Math.random() * len)].name;
 }
